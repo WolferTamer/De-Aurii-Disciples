cd50e681e822d096b7f296b71bc6639345277315
Caleb <wolfertamer@gmail.com>
2024-01-26 10:58:12 -0800

    Initial commit

---
+++ src/main/java/net/wolfertamer/auriidisciples/DeAuriiDisciplesClient.java
@@ -0,0 +1,10 @@
+package net.wolfertamer.auriidisciples;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeAuriiDisciplesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/wolfertamer/auriidisciples/DeAuriiDisciplesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wolfertamer.auriidisciples;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DeAuriiDisciplesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
